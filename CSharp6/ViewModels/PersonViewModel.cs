3ab3724994afa8ea96949be2a8d251e4341ff996
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-11-22 18:02:49 -0600

    MVVM example

---
+++ CSharp6/ViewModels/PersonViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+namespace CSharp6.ViewModels
+{
+    class PersonViewModel : INotifyPropertyChanged
+    {
+        private string firstName;
+
+        private string lastName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                // Old way of doing PropertyChanged notification
+                if (value != firstName)
+                {
+                    firstName = value;
+
+                    var handler = PropertyChanged;
+                    if (handler != null)
+                    {
+                        handler(this, new PropertyChangedEventArgs("FirstName"));
+                    }
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                // C# 6.0 way of doing PropertyChanged notification
+                if (value != lastName)
+                {
+                    lastName = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastName)));
+                }
+            }
+        }
+    }
+}
